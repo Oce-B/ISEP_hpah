5b34fe0bc706ea33624813cdf10980d67f5deb2f
Oce-B <oceane.brigitte-raffin@eleve.isep.fr>
2023-03-10 16:34:24 +0100

    Ajout variables House

--- src/main/java/org/example/House.java
+++ src/main/java/org/example/House.java
@@ -1,5 +1,14 @@
 package org.example;
 
 public class House {
+    private String houseName;
+    private String defenseBonus;
+
+    private String attackBonus;
+    private String potionBonus;
+    private String accuracyBonus;
+
+
+
 
 }
